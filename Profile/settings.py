86a405fedbbce6efdedc9a86a66fd523345a9d28
Nafiz Nahid <nafiz234.nn@gmail.com>
2024-11-06 02:15:00 +0600

    setting change

--- Profile/settings.py
+++ Profile/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2p5%nvkn&0($@kq944ja^t4kve%w2v8j@zkj)mg1en$m)k7n7g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
