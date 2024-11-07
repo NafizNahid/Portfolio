759e3a1b2ef45b50b498d804644c456cf01702cf
Nafiz Nahid <nafiz234.nn@gmail.com>
2024-11-07 19:17:43 +0600

    al

--- Profile/settings.py
+++ Profile/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2p5%nvkn&0($@kq944ja^t4kve%w2v8j@zkj)mg1en$m)k7n7g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,7 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     "main\static",
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- Profile/urls.py
+++ Profile/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 
+
 urlpatterns = [
     path('', include('main.urls')),
     path("admin/", admin.site.urls),
@@ -12,4 +13,4 @@ urlpatterns = [
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
